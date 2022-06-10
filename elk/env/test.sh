0e37d2e332ed12cd4de69404dbe30f649a9f50ab
v_beckdeng <v_beckdeng@tencent.com>
2022-06-10 11:53:22 +0800

    将配置文件更改为统一脚本配置

---
+++ elk/env/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export ES_HTTP_PORT=9200
+export ES_TCP_PORT=9300
+
+export Kibana_HTTP_PORT=5601
+
+export MySQL_PORT=3306
+export IP=192.168.255.10
+
+
+echo "自动替换环境变量中---"
+
+echo "edit kibana.yml ---"
+sed -i "s/\${IP}/$IP/g" ./kibana/kibana.yml
+sed -i "s/\${ES_HTTP_PORT}/$ES_HTTP_PORT/g" ./kibana/kibana.yml
+echo "finish kibana.yml ---"
+
+echo "edit logstash ---"
+sed -i "s/\${IP}/$IP/g" ./logstash/logstash.conf
+sed -i "s/\${ES_HTTP_PORT}/$ES_HTTP_PORT/g" ./logstash/logstash.conf
+sed -i "s/\${MySQL_PORT}/$MySQL_PORT/g" ./logstash/logstash.conf
+
+sed -i "s/\${IP}/$IP/g" ./logstash/logstash.yml
+sed -i "s/\${ES_HTTP_PORT}/$ES_HTTP_PORT/g" ./logstash/logstash.yml
+
+echo "finish logstash"
